0b89bfd5f652b4d2b80e1ea2cee9934e294dcacc
dso784 <nguyen.huynh@dision.tech>
2023-05-10 10:35:08 +0700

    update functional

--- src/components/Booking/partials/select/SelectDropdown.tsx
+++ src/components/Booking/partials/select/SelectDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Popover } from 'antd';
 
 const buttonWidth = 70;
@@ -6,19 +6,39 @@ import './SelectDropdown.module.scss';
 
 const SelectDropdown: React.FC = () => {
   const MAX = 4;
-  const initialState: number = 0;
-  const [adultValue, setAdultValue] = useState(initialState);
-  const [childrenValue, setChildrenValue] = useState(initialState);
+  const [adultValue, setAdultValue] = useState(0);
+  const [childrenValue, setChildrenValue] = useState(0);
+
+  // useEffect(() => {
+  //   if (adultValue + childrenValue > MAX) {
+  //     const diff = adultValue + childrenValue - MAX;
+  //     if (adultValue > diff) {
+  //       setAdultValue(adultValue + diff);
+  //       setChildrenValue(childrenValue - diff);
+  //     } else {
+  //       setAdultValue(1);
+  //       setChildrenValue(childrenValue - (diff - adultValue));
+  //     }
+  //   }
+  // }, [adultValue, childrenValue]);
 
   const handleIncrease = (mode: string) => {
-    mode === 'children'
-      ? setChildrenValue((prevState) => prevState + 1)
-      : setAdultValue((prevState) => prevState + 1);
+    if (mode === 'children' && adultValue + childrenValue < MAX) {
+      setChildrenValue(childrenValue + 1);
+    } else if (mode === 'adult' && adultValue < MAX) {
+      if (adultValue + childrenValue >= MAX) {
+        setChildrenValue(childrenValue - 1);
+      }
+      setAdultValue(adultValue + 1);
+    }
   };
+
   const handleDescrease = (mode: string) => {
-    mode === 'children'
-      ? setChildrenValue((prevState) => prevState - 1)
-      : setAdultValue((prevState) => prevState - 1);
+    if (mode === 'children' && childrenValue > 0) {
+      setChildrenValue(childrenValue - 1);
+    } else if (mode === 'adult' && adultValue > 0) {
+      setAdultValue(adultValue - 1);
+    }
   };
 
   const content = (
@@ -28,7 +48,7 @@ const SelectDropdown: React.FC = () => {
         <button
           disabled={adultValue <= 0}
           onClick={() => handleDescrease('adult')}
-          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:cursor-not-allowed"
+          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:opacity-50 disabled:cursor-not-allowed"
         >
           -
         </button>
@@ -36,7 +56,7 @@ const SelectDropdown: React.FC = () => {
         <button
           disabled={adultValue === MAX}
           onClick={() => handleIncrease('adult')}
-          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:cursor-not-allowed"
+          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:opacity-50 disabled:cursor-not-allowed"
         >
           +
         </button>
@@ -46,21 +66,22 @@ const SelectDropdown: React.FC = () => {
         <button
           disabled={childrenValue <= 0}
           onClick={() => handleDescrease('children')}
-          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:cursor-not-allowed"
+          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:opacity-50 disabled:cursor-not-allowed"
         >
           -
         </button>
         <p>{childrenValue}</p>
         <button
-          disabled={childrenValue + adultValue === MAX}
+          disabled={adultValue + childrenValue === MAX || adultValue === 0}
           onClick={() => handleIncrease('children')}
-          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:cursor-not-allowed"
+          className="w-10 h-10 border border-[#00b96b] rounded-[50%] disabled:opacity-50 disabled:cursor-not-allowed"
         >
           +
         </button>
       </div>
     </div>
   );
+
   return (
     <Popover placement="bottomLeft" content={content} trigger="click">
       <Button>{'Guests & Rooms'}</Button>

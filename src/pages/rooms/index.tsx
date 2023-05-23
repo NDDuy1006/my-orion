4a7da8f90982cc6df886a1e39c1a7f6d5a9a66b0
Thanh Tien <dso791>
2023-05-23 11:14:04 +0700

    update code

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -1,6 +1,6 @@
-import { NextSheetWidthLayout } from '@/types/layoutType'
-import React from 'react'
-import { GetServerSidePropsContext } from 'next';
+import { NextSheetWidthLayout } from '@/types/layoutType';
+import React from 'react';
+import { GetStaticPropsContext } from 'next';
 import { Wrapper } from '@/components';
 import CheckBox from '@/components/global/CheckBox';
 import hotel2 from '@/assets/hotel-2.png';
@@ -12,46 +12,46 @@ import RoomItem from '@/components/ResultItem/partials/RoomItem';
 import BookingLayout from '@/layouts/BookingLayout';
 import BookingStep from '@/components/global/BookingStep';
 import Recommended from '@/components/ResultItem/partials/Recommended';
+import axios from 'axios';
 
 const dataCheckBox = [
     {
         title: 'Amenities',
         content: [
-            { label: "24hr front desk", value: 'title1' },
-            { label: "Adults only", value: 'title2' },
-            { label: "Aire-conditioned", value: 'title3' },
-            { label: "Beachfront", value: 'title4' },
-            { label: "Business center", value: 'title5' },
-            { label: "label title 6", value: 'title6' },
-            { label: "label title 7", value: 'title7' },
-            { label: "label title 8", value: 'title8' },
-        ]
+            { label: '24hr front desk', value: 'title1' },
+            { label: 'Adults only', value: 'title2' },
+            { label: 'Aire-conditioned', value: 'title3' },
+            { label: 'Beachfront', value: 'title4' },
+            { label: 'Business center', value: 'title5' },
+            { label: 'label title 6', value: 'title6' },
+            { label: 'label title 7', value: 'title7' },
+            { label: 'label title 8', value: 'title8' },
+        ],
     },
     {
         title: 'Bed type',
         content: [
-            { label: "Queen", value: 'title1' },
-            { label: "Double", value: 'title2' },
-            { label: "King", value: 'title3' },
-            { label: "Single/Twin", value: 'title4' },
-            { label: "Bunk bed", value: 'title5' },
-            { label: "label title 6", value: 'title6' },
-            { label: "label title 7", value: 'title7' },
-            { label: "label title 8", value: 'title8' },
-            { label: "label title 9", value: 'title9' },
-            { label: "label title 10", value: 'title10' },
-        ]
+            { label: 'Queen', value: 'title1' },
+            { label: 'Double', value: 'title2' },
+            { label: 'King', value: 'title3' },
+            { label: 'Single/Twin', value: 'title4' },
+            { label: 'Bunk bed', value: 'title5' },
+            { label: 'label title 6', value: 'title6' },
+            { label: 'label title 7', value: 'title7' },
+            { label: 'label title 8', value: 'title8' },
+            { label: 'label title 9', value: 'title9' },
+            { label: 'label title 10', value: 'title10' },
+        ],
     },
     {
         title: 'Style',
-        content: []
+        content: [],
     },
     {
         title: 'Other',
-        content: []
+        content: [],
     },
-]
-
+];
 
 const roomsData = [
     {
@@ -59,7 +59,8 @@ const roomsData = [
         image: hotel1,
         favouriteRoom: true,
         roomName: 'Turmzimmer Suite',
-        description: 'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
+        description:
+            'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
         location: 'Sonnenhotel Zum Stern',
         propertiesRoom: {
             numOfPeople: 2,
@@ -73,7 +74,8 @@ const roomsData = [
         image: hotel2,
         favouriteRoom: false,
         roomName: 'Turmzimmer Suite',
-        description: 'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
+        description:
+            'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
         location: 'Sonnenhotel Zum Stern',
         propertiesRoom: {
             numOfPeople: 2,
@@ -87,7 +89,8 @@ const roomsData = [
         image: hotel3,
         favouriteRoom: true,
         roomName: 'Turmzimmer Suite',
-        description: 'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
+        description:
+            'Would you like to live at lofty heights in the old stately villa? With two bedrooms and a great view of the Wurmberg and the southern slope, this 57 sqm suite on the top floor is the right choice for those who love heights. Equipped with a kitchenette, two TVs and a bathroom with rain shower - but with a little less storage space due to the sloping roof. An open steam fireplace ensures atmospheric cosiness. The light atmosphere follows the daylight and you can change or dim it at any time. Mood themes can also be selected. We call it the "Titanic window": the deep room window with a grenade-like view of the southern Harz Mountains! And don\'t forget when the sun goes down: the snuggle switch by the bed!',
         location: 'Sonnenhotel Zum Stern',
         propertiesRoom: {
             numOfPeople: 2,
@@ -95,79 +98,92 @@ const roomsData = [
             acreage: 36,
         },
         price: 0,
-    }
-
-]
+    },
+];
 
 const stepData = [
     {
         step: 1,
-        stepName: 'Room and rate'
+        stepName: 'Room and rate',
     },
     {
         step: 2,
-        stepName: 'Extra Services'
+        stepName: 'Extra Services',
     },
     {
         step: 3,
-        stepName: 'Personal Information'
+        stepName: 'Personal Information',
     },
     {
         step: 4,
-        stepName: 'Payment'
+        stepName: 'Payment',
     },
     {
         step: 5,
-        stepName: 'Comfirmation'
-    }
-]
+        stepName: 'Comfirmation',
+    },
+];
 
 const RoomsPage: NextSheetWidthLayout = (props: RoomsPageProps) => {
-
+    console.log(props)
     return (
         <>
-
-            <Wrapper >
+            <Wrapper>
                 <BookingStep data={stepData} activeStep={2} />
-                <div className='grid grid-cols-12 gap-16 mt-10'>
-                    <div className='col-span-4 pr-5'>
+                <div className="grid grid-cols-12 gap-16 mt-10">
+                    <div className="col-span-4 pr-5">
                         <div>
-                            {
-                                dataCheckBox.map((ele: any, index: number) => {
-                                    return (
-                                        <CheckBox key={index} data={ele} />
-                                    )
-                                })
-                            }
+                            {dataCheckBox.map((ele: any, index: number) => {
+                                return <CheckBox key={index} data={ele} />;
+                            })}
 
-                            <Image className='w-full h-full mt-4' src={require('@/assets/banner-1.png')} alt='banner-hotel' width={0} height={0} />
+                            <Image
+                                className="w-full h-full mt-4"
+                                src={require('@/assets/banner-1.png')}
+                                alt="banner-hotel"
+                                width={0}
+                                height={0}
+                            />
                         </div>
                     </div>
 
-                    <div className='col-span-8'>
+                    <div className="col-span-8">
                         <Recommended />
-                        {
-                            roomsData.map((ele: any, index: any) => {
-                                return (
-                                    <RoomItem key={index} data={ele} />
-                                )
-                            })
-                        }
+                        {roomsData.map((ele: any, index: any) => {
+                            return <RoomItem key={index} data={ele} />;
+                        })}
                     </div>
                 </div>
-            </Wrapper></>
+            </Wrapper>
+        </>
     );
-}
+};
 
 RoomsPage.Layout = BookingLayout;
 
 export default RoomsPage;
 
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+    try {
+        const res = await axios.get('http://localhost:3000/api/rooms/VIE', {
+            params: {
+                page: 1,
+                perPage: 10,
+            },
+        });
 
-export const getStaticProps = async (context: GetServerSidePropsContext) => {
-
-    return {
-        props: {},
-        revalidate: 60
-    };
-}
+        return {
+            props: {
+                data:res.data
+            },
+            revalidate: 60,
+        }
+    } catch (err) {
+        return {
+            props: {
+                data: [],
+            },
+            revalidate: 0,
+        };
+    }
+};

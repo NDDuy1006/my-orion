9ec3c03b415fe8e87bfe231c569d1e96a010f320
dso784 <nguyen.huynh@dision.tech>
2023-05-23 18:16:16 +0700

    update

--- src/components/Booking/partials/DatePickerSelected/DatePickerSelect.tsx
+++ src/components/Booking/partials/DatePickerSelected/DatePickerSelect.tsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DatePicker } from 'antd';
 import { Dayjs } from 'dayjs';
-import DateRender, { DateRate } from './DateRender';
+import DateRender from './DateRender';
 import DateFooter from './DateFooter';
 
 const { RangePicker } = DatePicker;
 
 const DatePickerSelect = () => {
-  const [showPrice, setShowPrice] = useState<boolean>(true)
+    const [showPrice, setShowPrice] = useState<boolean>(true);
+    const [show, setShow] = useState(false);
 
-  const handleShowPrice = (checked: boolean) => {
-    setShowPrice(checked)
-  }
+    const handleShowPrice = (checked: boolean) => {
+        setShowPrice(checked);
+    };
 
-  return (
-    <RangePicker
-      className="rounded-[32px] h-12 w-[360px]"
-      placement="bottomLeft"
-      renderExtraFooter={() => <DateFooter onChange={handleShowPrice} />}
-      panelRender={(panalNode: React.ReactNode) => {
-        return <div className='w-[40vw]'>{panalNode}</div>
-      }}
-      onOpenChange={(value) => {
-        console.log(value);
-      }}
-      dateRender={(currentDate: Dayjs, today: Dayjs) => (
-        <DateRender showPrice={showPrice} currentDate={currentDate} today={today} />
-      )}
-    />
-  );
+    useEffect(() => {
+        const handleScroll = () => {
+            setShow(false);
+        };
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <RangePicker
+            open={show}
+            onClick={() => setShow(true)}
+            className="rounded-[32px] h-12 w-[360px]"
+            placement="bottomLeft"
+            renderExtraFooter={() => <DateFooter onChange={handleShowPrice} />}
+            panelRender={(panalNode: React.ReactNode) => {
+                return <div className="w-[40vw]">{panalNode}</div>;
+            }}
+            onOpenChange={(value) => {
+                console.log(value);
+            }}
+            dateRender={(currentDate: Dayjs, today: Dayjs) => (
+                <DateRender showPrice={showPrice} currentDate={currentDate} today={today} />
+            )}
+        />
+    );
 };
 
 export default DatePickerSelect;
--- src/components/Booking/partials/DatePickerSelected/DateRender.tsx
+++ src/components/Booking/partials/DatePickerSelected/DateRender.tsx
@@ -3,31 +3,31 @@ import { Dayjs } from 'dayjs';
 import clsx from 'clsx';
 
 interface DaterenderProps {
-  currentDate: Dayjs;
-  today?: Dayjs;
-  dateType?: string;
-  showPrice: boolean;
+    currentDate: Dayjs;
+    today?: Dayjs;
+    dateType?: string;
+    showPrice: boolean;
 }
 
 export enum DateRate {
-  high = 'high',
-  average = 'average',
-  low = 'low',
+    high = 'high',
+    average = 'average',
+    low = 'low',
 }
 
 const DateRender = ({ currentDate, today, dateType, showPrice }: DaterenderProps) => {
-  return (
-    <div
-      className={clsx('ant-picker-cell-inner !h-11 p-0 date', {
-        high: dateType === DateRate.high,
-        average: dateType === DateRate.average,
-        low: dateType === DateRate.low,
-      })}
-    >
-      <span className="text-xl leading-[10px] inline-block mt-2">{currentDate.date()}</span>
-      {showPrice && <p className="text-[12px]">30$</p>}
-    </div>
-  );
+    return (
+        <div
+            className={clsx('ant-picker-cell-inner !h-11 !w-11 p-0 !flex flex-col date', {
+                high: dateType === DateRate.high,
+                average: dateType === DateRate.average,
+                low: dateType === DateRate.low,
+            })}
+        >
+            <p className="text-[1.125rem]">{currentDate.date()}</p>
+            {showPrice && <p className="text-[0.625]">30$</p>}
+        </div>
+    );
 };
 
 export default DateRender;

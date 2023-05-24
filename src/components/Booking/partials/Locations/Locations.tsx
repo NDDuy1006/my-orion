5c215dbc174d681763deafc8b29c01fd021981b3
dso784 <nguyen.huynh@dision.tech>
2023-05-24 17:07:31 +0700

    update for Tien

--- src/components/Booking/partials/Locations/Locations.tsx
+++ src/components/Booking/partials/Locations/Locations.tsx
@@ -4,60 +4,63 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const Locations = () => {
-    const [valueSearch, setValueSearch] = useState();
-    const [locationData, setLocationData] = useState([]);
-    const [data] = useLocation();
+  const [valueSearch, setValueSearch] = useState<any>([]);
+  const [data] = useLocation();
 
-    useEffect(() => {
-        const locationData = data?.data.length
-            ? data?.data?.map((location: any) => {
-                  const child = location?.children.length
-                      ? location.children?.map((children: any) => {
-                            return {
-                                ...children,
-                                value: `${children.value}@${children.title}`,
-                            };
-                        })
-                      : [];
-                  return {
-                      ...location,
-                      children: child,
-                  };
-              })
-            : [];
-        setLocationData(locationData);
-    }, [data?.data]);
+  // useEffect(() => {
+  //     const locationData = data?.length
+  //         ? data?.map((location: any) => {
+  //               const child = location?.children.length
+  //                   ? location.children?.map((children: any) => {
+  //                         return {
+  //                             ...children,
+  //                             value: `${children.value}@${children.title}`,
+  //                         };
+  //                     })
+  //                   : [];
+  //               return {
+  //                   ...location,
+  //                   children: child,
+  //               };
+  //           })
+  //         : [];
+  //     setLocationData(locationData);
+  // }, [data?.data]);
 
-    return (
-        <TreeSelect
-            value={valueSearch}
-            className="mySelectIBE !w-[360px]"
-            treeDefaultExpandAll
-            switcherIcon={
-                <Image
-                    alt=""
-                    width={0}
-                    height={0}
-                    src={require('/public/images/icons/location.svg')}
-                />
-            }
-            suffixIcon={
-                <Image
-                    alt=""
-                    width={0}
-                    height={0}
-                    src={require('/public/images/icons/location.svg')}
-                />
-            }
-            showSearch
-            placeholder="Where would you like to go?"
-            treeData={locationData}
-            onSearch={(e) => console.log(e)}
-            onChange={(value: string) => {
-                setValueSearch(valueSearch);
-            }}
-        />
-    );
+  const handleSearch = (value: any) => {
+    for (let item of data) {
+      const res = item.children.filter((ele: any) => {
+        const keyWord = value.toLowerCase();
+        const title = ele.title.toLowerCase();
+
+        if (title.includes(keyWord)) {
+          return ele;
+        }
+      });
+      console.log(res);
+    }
+  };
+
+  return (
+    <TreeSelect
+      value={valueSearch}
+      className="mySelectIBE !w-[360px]"
+      treeDefaultExpandAll
+      switcherIcon={
+        <Image alt="" width={0} height={0} src={require('/public/images/icons/location.svg')} />
+      }
+      suffixIcon={
+        <Image alt="" width={0} height={0} src={require('/public/images/icons/location.svg')} />
+      }
+      showSearch
+      placeholder="Where would you like to go?"
+      treeData={data}
+      onSearch={handleSearch}
+      onChange={(value: string) => {
+        setValueSearch(valueSearch);
+      }}
+    />
+  );
 };
 
 export default Locations;

9f3ad26014a2b7593288c869fec2c52d0e34cf4e
dso784 <nguyen.huynh@dision.tech>
2023-05-30 10:10:36 +0700

    update json

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -6,31 +6,20 @@ import footerJson from '../../contents/footer';
 // console.log(defaultTheme.components?.Layout?.colorBgHeader);
 // style={{ background: defaultTheme.components?.Layout?.colorBgHeader }}
 
-// const links = [
-//   { link: 'Weingut Rommert', href: '' },
-//   { link: 'Ettershaus', href: '#' },
-//   { link: 'Huttmann', href: '' },
-//   { link: 'Zum Stern', href: '' },
-//   { link: 'Bayerischer Hof', href: '' },
-//   { link: 'Feldberg Am See', href: '' },
-//   { link: 'Weingut Rommert', href: '' },
-//   { link: 'Ettershaus', href: '/' },
-//   { link: 'Huttmann', href: '' },
-//   { link: 'Zum Stern', href: '' },
-//   { link: 'Bayerischer Hof', href: '' },
-//   { link: 'Feldberg Am See', href: '' },
-// ];
-
+const { footer, social, icon } = footerJson;
 const Footer = () => {
   return (
     <footer>
       <div className="flex gap-32">
-        {footerJson.footer?.map((col, idx) => (
+        {footer?.map((col, idx) => (
           <div key={idx}>
             <FooterCol title={col.title} links={col.links} />
           </div>
         ))}
-        <div></div>
+        <div>
+          <h3>{social.title}</h3>
+          <div></div>
+        </div>
       </div>
       <Brand />
     </footer>
